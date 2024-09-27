06f25209f8e02a3f46ee9bb0b807d5fca18b16ff
Kit Baroness <ddd@Cobra.DevCorp.org>
2024-09-27 01:00:14 -0400

    If you like this Project send a donation via Solana Blockchain cryptocurrency

--- js/walletqr.js
+++ js/walletqr.js
@@ -8,6 +8,6 @@ document.getElementById("ethereum-address").textContent = ethereumAddress;
 document.getElementById("solana-address").textContent = solanaAddress;
 
 // Update image paths for QR codes
-document.getElementById("bitcoin-qr").src = "../img/qr/btc.png";
-document.getElementById("ethereum-qr").src = "../img/qr/eth.png";
-document.getElementById("solana-qr").src = "../img/qr/sol.png"; 
+document.getElementById("bitcoin-qr").src = "https://github.com/DevCorpOrg/linktree/blob/main/img/qr/btc.png?raw=true";
+document.getElementById("ethereum-qr").src = "https://github.com/DevCorpOrg/linktree/blob/main/img/qr/eth.png?raw=true";
+document.getElementById("solana-qr").src = "https://github.com/DevCorpOrg/linktree/blob/main/img/qr/sol.png?raw=true"; 
